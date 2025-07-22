c22b027911fd90a862b4e0226d8552581f55cdfd
MUHAMMADMUBASHIR732 <muhammadmubashir8221@gmail.com>
2025-07-22 15:14:04 +0500

    .

--- leetcode.java
+++ leetcode.java
@@ -1,40 +1,103 @@
+import java.util.HashMap;
 
 public class leetcode {
-    
 
+    /*
+     * Given an array of integers nums and an integer target, return indices of the
+     * two numbers such that they add up to target.
+     * 
+     * You may assume that each input would have exactly one solution, and you may
+     * not use the same element twice.
+     * 
+     * You can return the answer in any order.
+     * 
+     * 
+     * 
+     * Example 1:
+     * 
+     * Input: nums = [2,7,11,15], target = 9
+     * Output: [0,1]
+     * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+     */
 
-/*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-
- 
-
-Example 1:
-
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
- */
-
- public static int[] twoSum(int[] nums, int target){
-    for (int i = 0; i < nums.length; i++) {
-        for (int j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                return new int[] { i, j };
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
             }
         }
+        return new int[] {};
     }
-    return new int[] {};
- }
- 
- public static void main(String[] args){
-    int[] nums = {3,2,4};
-    int target = 6;
-    int[] result = twoSum(nums, target);
-    System.out.println(result[0] + "  " + result[1]);
- }
+
+    /*
+     * ================= Q2 ======================
+     * Roman numerals are represented by seven different symbols: I, V, X, L, C, D
+     * and M.
+     * 
+     * Symbol Value
+     * I 1
+     * V 5
+     * X 10
+     * L 50
+     * C 100
+     * D 500
+     * M 1000
+     * For example, 2 is written as II in Roman numeral, just two ones added
+     * together. 12 is written as XII, which is simply X + II. The number 27 is
+     * written as XXVII, which is XX + V + II.
+     * 
+     * Roman numerals are usually written largest to smallest from left to right.
+     * However, the numeral for four is not IIII. Instead, the number four is
+     * written as IV. Because the one is before the five we subtract it making four.
+     * The same principle applies to the number nine, which is written as IX. There
+     * are six instances where subtraction is used:
+     * 
+     * I can be placed before V (5) and X (10) to make 4 and 9.
+     * X can be placed before L (50) and C (100) to make 40 and 90.
+     * C can be placed before D (500) and M (1000) to make 400 and 900.
+     * Given a roman numeral, convert it to an integer.
+     * 
+     * 
+     */
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int value = romanMap.get(s.charAt(i));
+            if (i + 1 < s.length()) {
+
+                int value2 = romanMap.get(s.charAt(i + 1));
+                if (value < value2) {
+                    total += (value2 - value);
+                    i++;
+                    continue;
+                }
+
+                total += value;
+            }
+
+        }
+
+        return total;
+    }
+
+
+
+    
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("IVII"));
+    }
 
 }
